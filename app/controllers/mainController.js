b22e05b3f2771eef67fef080f348f3ea528e4b33
Robin <robin.plancon@outlook.com>
2023-07-07 14:46:21 +0200

    adapt the main controller

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,39 +1,66 @@
 const dataMapper = require('../dataMapper');
+const { Pokemon, PokemonType } = require('../models');
 
 const mainController = {
   // méthode pour la page d'accueil
   homePage: async (req, res) => {
-    const pokedex = await dataMapper.getAllPokemon();
+    // on récupère tous les pokemons sans les types associés
+    const pokedex = await Pokemon.findAll();
+    // si aucun pokemon n'est trouvé, on affiche la page 404
     if (!pokedex) {
       res.status(404).render('404');
     }
+    // sinon on affiche la page d'accueil avec la liste des pokemons
     res.render('home', { pokedex });
   },
+  
   // méthode pour la page de détail d'un pokemon
   pokemonPage: async (req, res) => {
     const { id } = req.params;
-    const pokemon = await dataMapper.getPokemonById(id);
+
+    // on récupère le pokemon avec ses types associés
+    const pokemon = await Pokemon.findByPk(id, {
+      include: ['types'],
+    });
+
+    // si aucun pokemon n'est trouvé, on affiche la page 404
     if (!pokemon) {
       return res.status(404).render('404');
     }
-    const types = await dataMapper.getPokemonTypesByNumero(pokemon.numero);
-    return res.render('pokemonDetail', { pokemon, types });
+
+    // sinon on affiche la page de détail du pokemon
+    return res.render('pokemonDetail', { pokemon });
   },
+
   // méthode pour la page de recherche par type
   typesPage: async (req, res) => {
-    const types = await dataMapper.getAllTypes();
+    // on récupère tous les types de pokemon sans les pokemons associés
+    const types = await PokemonType.findAll();
+
+    // si aucun type n'est trouvé, on affiche la page 404
     if (!types) {
       res.status(404).render('404');
     }
+    
+    // sinon on affiche la page de recherche par type
     res.render('searchType', { types });
   },
+
   // méthode pour la page de recherche par type
   pokemonByTypePage: async (req, res) => {
     const { id } = req.params;
-    const pokemonList = await dataMapper.getPokemonByTypeId(id);
-    if (!pokemonList) {
+
+    // on récupère le type de pokemon avec les pokemons associés
+    const pokemonsOfType = await PokemonType.findByPk(id, {
+      include: ['pokemons'],
+    });
+    // si aucun type n'est trouvé, on affiche la page 404
+    if (!pokemonsOfType) {
       res.status(404).render('404');
     }
+    // sinon on garde uniquement la liste des pokemons associés au type
+    const pokemonList = pokemonsOfType.pokemons;
+    // on affiche la page des pokemons associés au type
     res.render('pokemonByType', { pokemonList });
   },
 };
